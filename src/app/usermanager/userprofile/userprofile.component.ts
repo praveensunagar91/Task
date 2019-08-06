e69ba2d3adeb2dfd21fc45f80ead37d3ef65d868
User1 <user1@Skycliffitdomain.com>
2019-08-06 18:39:54 +0530

    code push

--- src/app/usermanager/userprofile/userprofile.component.ts
+++ src/app/usermanager/userprofile/userprofile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-userprofile',
@@ -6,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userprofile.component.css']
 })
 export class UserprofileComponent implements OnInit {
+  Fname:string='';
+   Lname:string='';
+  Displayname:string='';
+  Gender:string='';
+   DOB:string='' ;
+   Address:string='';
+   City:string='';
+   State:string='';
+   Email:string='';
+  arr:User[]=[
+    new User('amar','akbar','amarakbar','male','01/03/1997','hubli','hubli','karnataka','amar@g.com')
 
+  ];
   constructor() { }
 
   ngOnInit() {
