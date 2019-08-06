46ce45afd8a0decc4b02535d41cf8b6dc808cc91
User1 <user1@Skycliffitdomain.com>
2019-08-06 15:26:41 +0530

    design added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserprofileComponent } from './usermanager/userprofile/userprofile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserprofileComponent
   ],
   imports: [
     BrowserModule
